2c0f280eb57ec2680902d0d3e11d0e2083f1ac73
Sam Lai <sam.lai@udnshopping.com>
2022-01-25 13:55:26 +0800

    Initial Commit

---
+++ SwiftUITableDemo/SwiftUITableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITableDemoApp.swift
+//  SwiftUITableDemo
+//
+//  Created by Sam Lai on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
